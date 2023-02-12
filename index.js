949bacd8763a97b471d956933a2befeacf66c954
Alexandra Romano <alex.romano80@gmail.com>
2023-02-12 11:51:32 -0700

    added images and toggling feature without hiding content

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const h3 = document.createElement('h3')
     const h4 = document.createElement('h4')
     const p = document.createElement('p')
+    const img = document.createElement('img')
 
 //Connecting HTML elements to array items
 
     h3.textContent = cocktail.strDrink;
-    // h4.textContent = (`${cocktail.strIngredient1}, ${cocktail.strIngredient2}, ${cocktail.strIngredient3}, ${cocktail.strIngredient4}`)
-    h4.textContent = deleteNull
+    h3.classList.add("margaritalist")
+    h4.classList.add("container")
+    h4.textContent = (`${cocktail.strIngredient1}, ${cocktail.strIngredient2}, ${cocktail.strIngredient3}, ${cocktail.strIngredient4}`)
     p.textContent = cocktail.strInstructions
+    img.src = cocktail.strDrinkThumb
   
 //appending the elements to the DOM 
 
@@ -24,14 +27,19 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#section').append(div)
 
 //1st Event Listener - Click Event     
-  
+
+
 h3.addEventListener("click",clickAlert);
 
- function clickAlert() {
+function clickAlert() {
   div.append(h4)
   div.append(p)
- }
-//Deleting Null values from Ingredients
+  div.append(img)
+  h4.classList.toggle('margaritalist')
+  p.classList.toggle('container')
+  }
+//Deleting Null values from Ingredients - Needs Work
+
 const deleteNull = {
   ingredientOne: cocktail.strIngredient1,
   ingredientTwo: cocktail.strIngredient2,
@@ -46,9 +54,8 @@ if (deleteNull[key] === null) {
   else {
     return deleteNull;
   }
+
 })
-
-
 
  //2nd Event Listener - Mouse Enter
 
@@ -87,12 +94,19 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
 .then(data => {
   cocktailDrinks = data.drinks.map(margarita => {
   const card = dataDrinkTemplate.content.cloneNode(true).children[0]
-  const header = card.querySelector("[ingredients]")
+  const header = card.querySelector("[cocktailname]")
+  const infoCard = card.querySelector("[ingredients]")
   const body = card.querySelector("[recipe]")
   header.textContent = margarita.strDrink
+  infoCard.textContent = (`${margarita.strIngredient1}, ${margarita.strIngredient2}, ${margarita.strIngredient3}, ${margarita.strIngredient4}`)
   body.textContent = margarita.strInstructions
   drinkCardContainer.append(card)
-  return { strDrink: margarita.strDrink, strInstructions: margarita.strInstructions, element: card }
+  return { strDrink: margarita.strDrink, Ingredient:margarita.strIngredient1,strInstructions: margarita.strInstructions, element: card }
   })
 })
 })
+
+//Notes: Need to attach function to h4 element
+// Need to attach images to drinks
+//Need to attach collapse feature for drinks
+
