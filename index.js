d4b978fb4a6a2e5e7e552418013b4dc94aefaa70
Alexandra Romano <alex.romano80@gmail.com>
2023-02-13 21:31:46 -0700

    tried to add function to get rid of null values

--- index.js
+++ index.js
@@ -17,7 +17,27 @@ document.addEventListener('DOMContentLoaded', () => {
     h3.textContent = cocktail.strDrink;
     h3.classList.add("margaritalist")
     h4.classList.add("container")
-    h4.textContent = (`${cocktail.strIngredient1}, ${cocktail.strIngredient2}, ${cocktail.strIngredient3}, ${cocktail.strIngredient4}`)
+    h4.textContent = (`${cocktail.strIngredient1}, ${cocktail.strIngredient2}, ${cocktail.strIngredient3}, ${cocktail.strIngredient4}, ${cocktail.strIngredient5}, ${cocktail.strIngredient6}, ${cocktail.strIngredient7}`)
+    h4.textContent = ingredients;
+const ingredientUl = document.createElement(ul)
+
+const ingredients = {
+  ingredientOne: cocktail.strIngredient1,
+  ingredientTwo: cocktail.strIngredient2,
+  ingredientThree: cocktail.strIngredient3,
+  ingredientFour: cocktail.strIngredient4,
+}
+
+for (const [key, value] in Object.entries(ingredients)) { 
+// Object.entries(ingredients) returns an array of arrays. Each nested array // will have 2 elements, the first is the key, the second the value, so we //can use destructuring to define a key and value variable
+
+  if (value) { 
+    const ingredientLi = document.createElement('li');
+    ingredientLi.textContent = value;
+    ul.appendChild(ingredientLi)
+  }
+  ul.appendChild(ingredientUl)
+}
     p.textContent = cocktail.strInstructions
     img.src = cocktail.strDrinkThumb
   
@@ -29,7 +49,16 @@ document.addEventListener('DOMContentLoaded', () => {
 //1st Event Listener - Click Event     
 
 
+
+
 h3.addEventListener("click",clickAlert);
+
+function logItem(e) {
+  const item = document.querySelector(`[container=${e.target.id}]`);
+  item.toggleAttribute('hidden');
+}
+
+h3.addEventListener('toggle', logItem)
 
 function clickAlert() {
   div.append(h4)
@@ -45,6 +74,9 @@ const deleteNull = {
   ingredientTwo: cocktail.strIngredient2,
   ingredientThree: cocktail.strIngredient3,
   ingredientFour: cocktail.strIngredient4,
+  ingredientFive: cocktail.strIngredient5,
+  ingredientSix: cocktail.strIngredient6,
+  ingredientSeven: cocktail.strIngredient7,
 }
 
 Object.keys(deleteNull).forEach(key => {
@@ -52,8 +84,9 @@ if (deleteNull[key] === null) {
     delete deleteNull[key];
   }
   else {
-    return deleteNull;
+    return {deleteNull};
   }
+  console.log(deleteNull)
 
 })
 
@@ -64,7 +97,7 @@ if (deleteNull[key] === null) {
   setTimeout(() => {
     event.target.style.color = "";
   }, 500);
-  }, false);
+  }, false)});
 
 })
 })
@@ -104,9 +137,8 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
   return { strDrink: margarita.strDrink, Ingredient:margarita.strIngredient1,strInstructions: margarita.strInstructions, element: card }
   })
 })
-})
+
 
 //Notes: Need to attach function to h4 element
-// Need to attach images to drinks
 //Need to attach collapse feature for drinks
 
